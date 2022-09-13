d28889e26fa93dd384d60b839a492fceb1e55548
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-09-13 17:44:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -173,15 +173,15 @@ export async function run() {
       for (const l of lines) {
         try {
           const line = l.match(regex);
-          if (!line[1] || line[2] && line[2].length == 0) return;
-          const s = parse_rfc3339(line[1]);
-          const xlog = { "timestamp": s || Date.now(), "message": line[2] || '' }
+          if (!line[1] || (line[2] && line[2].length === 0)) return;
+          const s = parse_rfc3339(line[1]) || Date.now();
+          const xlog = { "timestamp": s, "message": line[2] }
           core.debug(`${xlog}`);
           logs.info(xlog);
         } catch(e) { 
-          core.debug(`${l}`);
           const xlog = { "timestamp": Date.now(), "message": l }
           logs.info(xlog); 
+          core.debug(`error: ${e}`);
         }
       }
       logs.clear();
